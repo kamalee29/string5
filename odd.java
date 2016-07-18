90673161a1689b81e88f56d9ef43e526046f3215
kamalee29 <kamalee.selvii24@gmail.com>
2016-07-18 05:51:38 -0700

    Add files via upload

---
+++ odd.java
@@ -0,0 +1,14 @@
+
+    int n,i,a,b;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter the starting element");
+        a=sc.nextInt();
+          System.out.println("enter the ending element");
+          b=sc.nextInt();
+        for(i=a;i<=b;i++){
+        if(i%2==0)
+           
+        {continue;}
+        else{  System.out.print(i);
+        }}
+    }
