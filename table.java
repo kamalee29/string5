bdd8a3cd949d254d646c5a625f7f7716a9302d2b
kamalee29 <kamalee.selvii24@gmail.com>
2016-07-18 05:54:03 -0700

    Add files via upload

---
+++ table.java
@@ -0,0 +1,6 @@
+ int i,n,n1;
+        System.out.println("enter the table number do you want");
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+        for(i=1;i<16;i++){
+            System.out.println(i+"*"+n+"="+(i*n));
